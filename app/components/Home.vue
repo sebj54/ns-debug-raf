63ee65ff92bb7d40ec443fc800d2f693fe5c5bd8
Sébastien JEAN <contact@sebjean.com>
2022-08-02 14:56:10 +0200

    ✨ Use requestAnimationFrame

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -5,24 +5,33 @@
         </ActionBar>
 
         <GridLayout>
-            <Label class="info">
-                <FormattedString>
-                    <Span class="fas" text.decode="&#xf135; "/>
-                    <Span :text="message"/>
-                </FormattedString>
-            </Label>
+            <Label class="info" :text="lastTimestamp" />
         </GridLayout>
     </Page>
 </template>
 
 <script>
-  export default {
-    computed: {
-      message() {
-        return "Blank {N}-Vue app";
-      }
-    }
-  };
+import {
+    requestAnimationFrame,
+    cancelAnimationFrame,
+} from '@nativescript/core/animation-frame'
+
+export default {
+    data() {
+        return {
+            lastTimestamp: 0,
+        }
+    },
+    mounted() {
+        requestAnimationFrame(this.updateTimestamp)
+    },
+    methods: {
+        updateTimestamp(timestamp) {
+            this.lastTimestamp = timestamp
+            requestAnimationFrame(this.updateTimestamp)
+        },
+    },
+}
 </script>
 
 <style scoped lang="scss">
