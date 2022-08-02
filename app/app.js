e5a989bc6102056def434c56b1b9e1dac1b8e8d0
Sébastien JEAN <contact@sebjean.com>
2022-08-02 14:57:11 +0200

    💡 Add comments in app.js about crash prevention

--- app/app.js
+++ app/app.js
@@ -2,6 +2,9 @@ import Vue from 'nativescript-vue'
 
 import Home from './components/Home'
 
+// Uncomment the line below to prevent the app from crashing
+// global.__runOnMainThread = undefined
+
 new Vue({
   render: (h) => h('frame', [h(Home)]),
 }).$start()
